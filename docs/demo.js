37dba5ac07a381224465fec2f094181e66977e7b
Collin <collinclarke@gmail.com>
2017-11-23 13:11:33 -0500

    Add true randomness

--- docs/demo.js
+++ docs/demo.js
@@ -10,27 +10,31 @@ cshr(() => {
 
   window.categorySelector = categorySelector;
 
-  let listing, requestJSON;
-  listing = 155597;
-  const url = "https://api.harvardartmuseums.org/object";
-  const request = () => ({
-    url,
-    // url: url + parseInt(listing),
-    data: {
-      apikey: "5396a200-c4f2-11e7-8f8e-1b14c6858050",
-      totalpageviews: 0,
-      size: 1
-    },
-    contentType: "application/json"
-  });
+  let section, requestJSON;
+  objectID = Math.floor(Math.random() * 10000);
+  section = "images";
+
+  const request = () => {
+    console.log(objectID);
+    return ({
+      url: "https://api.harvardartmuseums.org/object/" + objectID,
+      data: {
+        apikey: "5396a200-c4f2-11e7-8f8e-1b14c6858050",
+        // size: 1,
+        // hasimage: 1,
+        // page: 1
+      },
+      contentType: "application/json"
+    });
+  };
 
   img.on("load", (e) => {
     loading.addClass("hidden");
   });
 
   back.on("click", (e) => {
-    if (listing > 1) {
-      listing -= 1;
+    if (objectID > 1) {
+      objectID -= 1;
     }
     loading.removeClass("hidden");
 
@@ -39,10 +43,10 @@ cshr(() => {
       response = JSON.parse(response);
       img.attr("src", response.images[0].baseimageurl);
       info.empty();
-      response.images[0].clarifai.outputs[0].data.concepts.forEach(concept => {
-        const conceptText = concept.name;
-        info.append(cshr(`<li>${conceptText}</li>`));
-      });
+      // response.images[0].clarifai.outputs[0].data.concepts.forEach(concept => {
+      //   const conceptText = concept.name;
+      //   info.append(cshr(`<li>${conceptText}</li>`));
+      // });
     })
     .catch(reason => {
       console.log('Handle rejected promise ('+reason+') here');
@@ -50,17 +54,17 @@ cshr(() => {
   });
 
   next.on("click", (e) => {
-    listing += 1;
+    objectID += 1;
     loading.removeClass("hidden");
     requestJSON = request();
     cshr.ajax(requestJSON).then(response => {
       response = JSON.parse(response);
-      debugger
+      // debugger
       img.attr("src", response.images[0].baseimageurl);
       info.empty();
-      response.images[0].clarifai.outputs[0].data.concepts.forEach(concept => {
-        info.append(concept.name);
-      });
+      // response.images[0].clarifai.outputs[0].data.concepts.forEach(concept => {
+      //   info.append(concept.name);
+      // });
     })
     .catch(reason => {
       console.log('Handle rejected promise ('+reason+') here');
